08fcdf635d6a08431690f22bf5928074df63a7e2
Hashim Sharif <sharif.hashim@gmail.com>
2022-07-07 16:47:01 -0400

    require bcrypt and register endpoint

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const dbConnect = require("./db/dbConnect");
+const User = require("./db/userModel");
+const bcrypt = require("bcrypt");
 
 dbConnect();
 
@@ -12,6 +14,41 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", (request, response, next) => {
   response.json({ message: "Hey! This is your server response!" });
   next();
+});
+
+app.post("/register", (request, response) => {
+  //hash the password
+  bcrypt
+    .hash(request.body.password, 10)
+    // create a new user instance and collect the data
+    .then((hashedPassword) => {
+      const user = new User({
+        email: request.body.email,
+        password: hashedPassword,
+      });
+      // save the user
+      user
+        .save()
+        .then((result) => {
+          response.status(201).send({
+            message: "User Created Successfully",
+            result,
+          });
+        })
+        .catch((error) => {
+          response.status(500).send({
+            message: "Error creating user",
+            error,
+          });
+        });
+    })
+    // catch error if the password hash isn't successful
+    .catch((e) => {
+      response.status(500).send({
+        message: "Password was not hashed successfully",
+        e,
+      });
+    });
 });
 
 module.exports = app;
