c098075907e416515e93f0a1a0e001f3725f1a8f
Hashim Sharif <sharif.hashim@gmail.com>
2022-07-07 17:28:41 -0400

    create login route and assign jwt

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const dbConnect = require("./db/dbConnect");
 const User = require("./db/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 dbConnect();
 
@@ -46,6 +47,48 @@ app.post("/register", (request, response) => {
     .catch((e) => {
       response.status(500).send({
         message: "Password was not hashed successfully",
+        e,
+      });
+    });
+});
+
+app.post("/login", (request, response) => {
+  User.findOne({ email: request.body.email })
+    .then((user) => {
+      bcrypt
+        .compare(request.body.password, user.password)
+        .then((passwordCheck) => {
+          //check if password matches
+          if (!passwordCheck) {
+            return response.status(400).send({
+              message: "Passwords does not match",
+              error,
+            });
+          }
+          const token = jwt.sign(
+            {
+              userId: user._id,
+              userEmail: user.email,
+            },
+            "RANDOM-TOKEN",
+            { expiresIn: "24h" }
+          );
+          response.status(200).send({
+            message: "Login Successful",
+            email: user.email,
+            token,
+          });
+        })
+        .catch((error) => {
+          response.status(400).send({
+            message: "Passwords does not match",
+            error,
+          });
+        });
+    })
+    .catch((e) => {
+      response.status(404).send({
+        message: "Email not found",
         e,
       });
     });
